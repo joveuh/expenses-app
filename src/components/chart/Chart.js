19e2d3a373060007a6049d03a70630df5b03e6a6
Uzair H <uzair@ibm.com>
2022-08-26 14:52:43 -0400

    Working Chart Bar Graph

--- src/components/chart/Chart.js
+++ src/components/chart/Chart.js
@@ -1,13 +1,19 @@
 import React from "react";
 import ChartBar from "./ChartBar";
+import './Chart.css';
 
 const Chart = (props) => {
+  const dataPointMonthValues = props.dataPoints.map(
+    (dataPoint) => dataPoint.value
+  );
+  const totalMaximum = Math.max(... dataPointMonthValues);
+
   return (
     <div className="chart">
       {props.dataPoints.map((datapoint) => (
         <ChartBar
           value={datapoint.value}
-          maxValue={null}
+          maxValue={totalMaximum}
           label={datapoint.label}
           key={datapoint.label}
         />
